6a6e7d13748d6cd706a3b0ad7479b0a2399bc3e6
Soren Skov Jensen <soren.skov.jensen@gmail.com>
2022-11-17 15:24:27 +0100

    roundGrade function implemented

--- roundgrade.py
+++ roundgrade.py
@@ -1,7 +1,14 @@
-def roundGrade(grades):
-    #insert code here
+import numpy as np
+import pandas as pd
+
+
+def roundGrade(grades: np.array):
     
-    gradesRounded = []
+    possible_grades = np.array([-3, 0, 2, 4, 7, 10, 12])
+    gradesRounded = np.array([])
+    
+    for grade in grades:
+        gradesRounded = np.append(gradesRounded, min(possible_grades, key=lambda x: abs(x - grade)))
     
     return gradesRounded
 
