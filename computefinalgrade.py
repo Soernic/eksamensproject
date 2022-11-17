e795a9ebbc4739b37e59c946c581af11826fa620
Malte Lau <malte.lau@live.dk>
2022-11-17 14:14:33 +0100

    Final grade

--- computefinalgrade.py
+++ computefinalgrade.py
@@ -1,3 +1,10 @@
-def computeFinalGrades(grades):
-    gradesFinal = None
+def computeFinalGrades(gradesRounded):
+    gradesFinal = []
+    for i in range(0, len(gradesRounded)):
+        if len(gradesRounded[i]) == 1:
+            gradesFinal.append(gradesRounded[i][0])
+        else:
+            gradesFinal.append((sum(gradesRounded)-min(gradesRounded))/len(gradesRounded))
+        gradesFinal.append(gradesRounded[i])
+    
     return gradesFinal
