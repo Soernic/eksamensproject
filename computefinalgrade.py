a63e357761f74ee146a9d300f8380afe1e2d7ea8
AugustBorgLjorring <s224178@student.dtu.dk>
2022-11-17 15:00:31 +0100

    ComputeFinalGradeUpdate

--- computefinalgrade.py
+++ computefinalgrade.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 def computeFinalGrades(grades):
-    gradesFinal = np.zeros(len(grades, axis=0))
-    for i in range(0,len(grades)):
+    gradesFinal = np.zeros(len(grades))
+    
+    for i in range(len(grades)):
         if -3 in grades[i]:
             gradesFinal[i] = -3
-        elif len(grades) == 1:
-            gradesFinal[i] = grades[i]
+        elif len(grades[i]) == 1:
+            gradesFinal[i] = grades[i][0]
         else:
-            gradesFinal[i] = np.mean(grades[i]-min(grades[i]))
+            grades[i].remove(np.min(grades[i]))
+            gradesFinal[i] = np.mean(grades[i])
     return gradesFinal
 
 print(computeFinalGrades(np.array([[10,12,7,4],[12,10],[7,4,-3],[2],[-3]])))
