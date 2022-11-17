8a34707cfdd922cbdeec57e1b4ce195742cc670c
Soren Skov Jensen <soren.skov.jensen@gmail.com>
2022-11-17 15:26:27 +0100

    function description added

--- roundgrade.py
+++ roundgrade.py
@@ -3,7 +3,16 @@ import pandas as pd
 
 
 def roundGrade(grades: np.array):
+    """_summary_
+
+    Args:
+        grades (np.array): _description_
+
+    Returns:
+        _type_: _description_
+    """
     
+    # Function takes vector of grades and rounds them to the nearest appropriate grade to correct for potential data errors.
     possible_grades = np.array([-3, 0, 2, 4, 7, 10, 12])
     gradesRounded = np.array([])
     
